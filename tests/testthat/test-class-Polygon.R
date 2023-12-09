b02b1d05fb8c70258e767f4f8e976e400620326b
Chun-Hui Gao <gaospecial@gmail.com>
2023-12-09 22:20:01 +0800

    add more test

--- tests/testthat/test-class-Polygon.R
+++ tests/testthat/test-class-Polygon.R
@@ -1,4 +1,5 @@
-test_that("discern_overlap(Polygon) works", {
+test_that("Polygon methods works", {
+  # generate sample data
   parameters <- list(c(0.35, 0.47, 0.35, 0.20, 135),
                      c(0.50, 0.57, 0.35, 0.15, 135),
                      c(0.50, 0.57, 0.33, 0.15,  45),
@@ -9,7 +10,20 @@ test_that("discern_overlap(Polygon) works", {
   polygons <- lapply(ellipses,function(x) sf::st_polygon(list(as.matrix(x))))
   polygon <- Polygon(sets = polygons)
 
-  overlap1 <- discern_overlap(polygon)
-  overlap2 <- purrr::reduce(polygon@sets, function(x,y) sf::st_intersection(x,y))
-  expect_equal(overlap1,overlap2)
+  # test discern_overlap()
+  discern_overlap1 <- discern_overlap(polygon) # the unique (overlapping) region of four elllipses
+  discern_overlap2 <- Reduce(sf::st_intersection, polygon@sets)
+  discern_overlap3 = overlap(polygon)
+  expect_equal(discern_overlap1, discern_overlap2)
+  expect_equal(discern_overlap1, discern_overlap3)
+
+  # test discern
+  discern1 = discern(polygon, 1:2, 3:4) # the region `slice1` has but `slice2` doest have.
+  discern2 = sf::st_difference(sf::st_union(polygons[[1]], polygons[[2]]),sf::st_union(polygons[[3]], polygons[[4]]))
+  expect_equal(discern1, discern2)
+
+  # test overlap
+  overlap1 = overlap(polygon)  # the overlaping region of four ellipses
+  overlap2 = Reduce(sf::st_intersection, polygons)
+  expect_equal(overlap1, overlap2)
 })
---
+++ tests/testthat/test-class-VennPlotData.R
@@ -0,0 +1,21 @@
+test_that("VennPlotData methods work", {
+  set_Edge = fancy_2d_circle()
+  v = VennPlotData(
+    shapeId = "1",
+    type = "circle",
+    setEdge = set_Edge,
+    setLabel = fancy_2d_circle_label()
+  )
+  expect_s4_class(v, "VennPlotData")
+  expect_equal(v@nsets, 2L)
+
+  # test intersection
+  overlap1 = sf::st_intersection(sf::st_polygon(list(set_Edge[[1]])), sf::st_polygon(list(set_Edge[[2]])))
+  overlap2 = v@regionEdge$geometry[[3]]
+  expect_equal(overlap1, overlap2)
+
+  # test centrorid
+  center1 = sf::st_centroid(overlap1)
+  center2 = v@regionLabel$geometry[[3]]
+  expect_equal(center1, center2)
+})
