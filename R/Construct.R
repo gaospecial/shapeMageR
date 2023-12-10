41830d4fb55884786911907dc81806092611460f
Chun-Hui Gao <gaospecial@gmail.com>
2023-12-10 10:07:42 +0800

    VennPlotData doc

--- R/Construct.R
+++ R/Construct.R
@@ -47,11 +47,12 @@ setMethod("Polygon", c(sets = "ANY"),
 #' An S4 class to represent Venn plot components.
 #'
 #' @slot shapeId shape id
-#' @slot setEdge a list of coordinates matrix defining Venn set edges
-#' @slot region the feature region will be calculated automatically with `setEdge`
 #' @slot type type of shape
 #' @slot nsets number of sets
+#' @slot setEdge a list of coordinates matrix defining Venn set edges
 #' @slot setLabel label of sets
+#' @slot regionEdge the feature region will be calculated automatically with `setEdge`
+#' @slot regionLabel the centroid of region, where region label appears
 setClass("VennPlotData",
          slots = c(shapeId = "ANY",
                    type = "ANY",
